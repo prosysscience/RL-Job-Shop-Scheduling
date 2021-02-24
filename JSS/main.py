18ec58357550ed405a174a0ac2fa3be5b8b08674
Pierre TASSEL <pierre.tassel@outlook.fr>
2021-02-24 14:26:34 +0100

    first working config

--- JSS/main.py
+++ JSS/main.py
@@ -8,8 +8,10 @@ import random
 import numpy as np
 
 import ray.tune.integration.wandb as wandb_tune
+from ray import tune
+from ray.rllib.agents.dqn import ApexTrainer
 
-from ray.rllib.agents.ppo import PPOTrainer
+from ray.rllib.agents.dqn.dqn import DEFAULT_CONFIG as DQN_CONFIG, DQNTrainer
 
 from CustomCallbacks import *
 from models import *
@@ -20,7 +22,7 @@ import multiprocessing as mp
 from ray.rllib.agents import with_common_config
 from ray.rllib.models import ModelCatalog
 
-from ray.tune.utils import flatten_dict
+from ray.tune.utils import flatten_dict, merge_dicts
 from ray.rllib.utils.framework import try_import_tf
 
 tf1, tf, tfv = try_import_tf()
@@ -62,43 +64,122 @@ def train_func():
     default_config = {
         'env': 'JSSEnv:jss-v1',
         'seed': 0,
-        'framework': 'tf',
+        'framework': 'torch',
         'log_level': 'WARN',
-        'num_gpus': 1,
+        'num_gpus': 0,
         'instance_path': 'instances/ta41',
         'evaluation_interval': None,
         'metrics_smoothing_episodes': 2000,
-        'gamma': 1.0,
-        'num_workers': mp.cpu_count(),
+        #'num_workers': mp.cpu_count(),
         'layer_nb': 2,
-        'train_batch_size': mp.cpu_count() * 4 * 704,
-        'num_envs_per_worker': 4,
-        'rollout_fragment_length': 704,  # TO TUNE
-        'sgd_minibatch_size': 33000,
+        #'train_batch_size': mp.cpu_count() * 4 * 704,
+        'num_envs_per_worker': 1,
         'layer_size': 319,
         'lr': 0.0006861,  # TO TUNE
         'lr_start': 0.0006861,  # TO TUNE
         'lr_end': 0.00007783,  # TO TUNE
-        'clip_param': 0.541,  # TO TUNE
-        'vf_clip_param': 26,  # TO TUNE
-        'num_sgd_iter': 12,  # TO TUNE
-        "vf_loss_coeff": 0.7918,
-        "kl_coeff": 0.496,
-        'kl_target': 0.05047,  # TO TUNE
-        'lambda': 1.0,
-        'entropy_coeff': 0.0002458,  # TUNE LATER
-        'entropy_start': 0.0002458,
-        'entropy_end': 0.002042,
-        'entropy_coeff_schedule': None,
-        "batch_mode": "truncate_episodes",
-        "grad_clip": None,
-        "use_critic": True,
-        "use_gae": True,
-        "shuffle_sequences": True,
-        "vf_share_layers": False,
         "observation_filter": "NoFilter",
-        "simple_optimizer": False,
-        "_fake_gpus": False,
+
+            # === Model ===
+        # Number of atoms for representing the distribution of return. When
+        # this is greater than 1, distributional Q-learning is used.
+        # the discrete supports are bounded by v_min and v_max
+        "num_atoms": 1,
+        "v_min": -10.0,
+        "v_max": 10.0,
+        # Whether to use noisy network
+        "noisy": False,
+        # control the initial value of noisy nets
+        "sigma0": 0.5,
+        # Whether to use dueling dqn
+        "dueling": False,
+        # Dense-layer setup for each the advantage branch and the value branch
+        # in a dueling architecture.
+        "hiddens": [],
+        # Whether to use double dqn
+        "double_q": True,
+        # N-step Q learning
+        "n_step": 1,
+
+        # === Exploration Settings ===
+        "exploration_config": {
+            # The Exploration class to use.
+            "type": "EpsilonGreedy",
+            # Config for the Exploration class' constructor:
+            "initial_epsilon": 1.0,
+            "final_epsilon": 0.02,
+            "epsilon_timesteps": 10000,  # Timesteps over which to anneal epsilon.
+
+            # For soft_q, use:
+            # "exploration_config" = {
+            #   "type": "SoftQ"
+            #   "temperature": [float, e.g. 1.0]
+            # }
+        },
+        # Switch to greedy actions in evaluation workers.
+        "evaluation_config": {
+            "explore": False,
+        },
+
+        # Minimum env steps to optimize for per train call. This value does
+        # not affect learning, only the length of iterations.
+        "timesteps_per_iteration": 100,
+        # Update the target network every `target_network_update_freq` steps.
+        "target_network_update_freq": 500,
+        # === Replay buffer ===
+        # Size of the replay buffer. Note that if async_updates is set, then
+        # each worker will have a replay buffer of this size.
+        "buffer_size": 50000,
+        # If True prioritized replay buffer will be used.
+        "prioritized_replay": True,
+        # Alpha parameter for prioritized replay buffer.
+        "prioritized_replay_alpha": 0.6,
+        # Beta parameter for sampling from prioritized replay buffer.
+        "prioritized_replay_beta": 0.4,
+        # Final value of beta (by default, we use constant beta=0.4).
+        "final_prioritized_replay_beta": 0.4,
+        # Time steps over which the beta parameter is annealed.
+        "prioritized_replay_beta_annealing_timesteps": 20000,
+        # Epsilon to add to the TD errors when updating priorities.
+        "prioritized_replay_eps": 1e-6,
+        # Whether to LZ4 compress observations
+        "compress_observations": False,
+        # Callback to run before learning on a multi-agent batch of experiences.
+        "before_learn_on_batch": None,
+        # If set, this will fix the ratio of replayed from a buffer and learned on
+        # timesteps to sampled from an environment and stored in the replay buffer
+        # timesteps. Otherwise, the replay will proceed at the native ratio
+        # determined by (train_batch_size / rollout_fragment_length).
+        "training_intensity": None,
+
+        # === Optimization ===
+        # Learning rate for adam optimizer
+        #"lr": 5e-4,
+        # Learning rate schedule
+        "lr_schedule": None,
+        # Adam epsilon hyper parameter
+        "adam_epsilon": 1e-8,
+        # If not None, clip gradients during optimization at this value
+        "grad_clip": 40,
+        # How many steps of the model to sample before learning starts.
+        "learning_starts": 1000,
+        # Update the replay buffer with this many samples at once. Note that
+        # this setting applies per-worker if num_workers > 1.
+        "rollout_fragment_length": 4,
+        # Size of a batch sampled from replay buffer for training. Note that
+        # if async_updates is set, then each worker returns gradients for a
+        # batch of this size.
+        "train_batch_size": 32,
+
+        # === Parallelism ===
+        # Number of workers for collecting samples with. This only makes sense
+        # to increase if your environment is particularly slow to sample, or if
+        # you"re using the Async or Ape-X optimizers.
+        "num_workers": 0,
+        # Whether to compute priorities on workers.
+        "worker_side_prioritization": False,
+        # Prevent iterations from going lower than this time span
+        "min_iter_time_s": 1,
     }
 
     wandb.init(config=default_config)
@@ -110,42 +191,39 @@ def train_func():
     config = wandb.config
 
     ModelCatalog.register_custom_model("fc_masked_model_tf", FCMaskedActionsModelTF)
+    ModelCatalog.register_custom_model("fc_masked_model_torch", FCMaskedActionsModelTorch)
 
     config['model'] = {
         "fcnet_activation": "relu",
-        "custom_model": "fc_masked_model_tf",
+        "custom_model": "fc_masked_model_torch",
         'fcnet_hiddens': [config['layer_size'] for k in range(config['layer_nb'])],
-        "vf_share_layers": False,
     }
+
     config['env_config'] = {
         'env_config': {'instance_path': config['instance_path']}
     }
 
     config = with_common_config(config)
     config['seed'] = 0
-    config['callbacks'] = CustomCallbacks
-    config['train_batch_size'] = config['sgd_minibatch_size']
+    #config['callbacks'] = CustomCallbacks
+    #config['train_batch_size'] = config['sgd_minibatch_size'] 33000
 
     config['lr'] = config['lr_start']
-    config['lr_schedule'] = [[0, config['lr_start']], [15000000, config['lr_end']]]
-
-    config['entropy_coeff'] = config['entropy_start']
-    config['entropy_coeff_schedule'] = [[0, config['entropy_start']], [15000000, config['entropy_end']]]
+    #config['lr_schedule'] = [[0, config['lr_start']], [15000000, config['lr_end']]]
 
     config.pop('instance_path', None)
     config.pop('layer_size', None)
     config.pop('layer_nb', None)
     config.pop('lr_start', None)
     config.pop('lr_end', None)
-    config.pop('entropy_start', None)
-    config.pop('entropy_end', None)
 
     stop = {
         "time_total_s": 10 * 60,
     }
 
     start_time = time.time()
-    trainer = PPOTrainer(config=config)
+    #tune_var = tune.run("DQN", config=config, stop=stop, verbose=1)
+    trainer = DQNTrainer(config=config)
     while start_time + stop['time_total_s'] > time.time():
         result = trainer.train()
         result = wandb_tune._clean_log(result)
--- JSS/models.py
+++ JSS/models.py
@@ -1,13 +1,15 @@
 import gym
-
-from ray.rllib.utils.framework import try_import_tf
+from ray.rllib.agents.dqn.dqn_torch_model import DQNTorchModel
 
 from ray.rllib.agents.dqn.distributional_q_tf_model import DistributionalQTFModel
 from ray.rllib.models.tf.fcnet import FullyConnectedNetwork
 from ray.rllib.models.tf.tf_modelv2 import TFModelV2
-
+from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
+from ray.rllib.utils.framework import try_import_tf, try_import_torch
+from ray.rllib.utils.torch_ops import FLOAT_MIN, FLOAT_MAX
 
 tf1, tf, tfv = try_import_tf()
+torch, nn = try_import_torch()
 
 class FCMaskedActionsModelTF(DistributionalQTFModel, TFModelV2):
     def __init__(self,
@@ -35,6 +37,37 @@ class FCMaskedActionsModelTF(DistributionalQTFModel, TFModelV2):
         #inf_mask = tf.maximum(tf.math.log(action_mask), tf.float32.min)
         logits = tf.where(tf.math.equal(action_mask, 1), raw_actions,  tf.float32.min)
         return logits, state
+
+    def value_function(self):
+        return self.action_embed_model.value_function()
+
+class FCMaskedActionsModelTorch(DQNTorchModel):
+    """PyTorch version of above ParametricActionsModel."""
+
+    def __init__(self,
+                 obs_space,
+                 action_space,
+                 num_outputs,
+                 model_config,
+                 name,
+                 **kw):
+        DQNTorchModel.__init__(self, obs_space, action_space, num_outputs,
+                               model_config, name, **kw)
+
+        true_obs_space = gym.spaces.MultiBinary(n=obs_space.shape[0] - action_space.n)
+        self.action_embed_model = TorchFC(
+            obs_space=true_obs_space, action_space=action_space, num_outputs=action_space.n,
+            model_config=model_config, name=name + "action_model")
+
+    def forward(self, input_dict, state, seq_lens):
+        action_mask = input_dict["obs"]["action_mask"]
+
+        # Compute the predicted action embedding
+        raw_actions, _ = self.action_embed_model({
+            "obs": input_dict["obs"]["real_obs"]
+        })
+        # inf_mask = tf.maximum(tf.math.log(action_mask), tf.float32.min)
+        return raw_actions.masked_fill_(action_mask == 0, FLOAT_MIN), state
 
     def value_function(self):
         return self.action_embed_model.value_function()
